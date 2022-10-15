134ea55a043c520cd9ed216e752b5b05eab5581a
tinashenatty <nattymachona2000@gmail.com>
2022-10-15 06:03:33 -0700

    Task 2 update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/*
- * main -prints output alphabets in lower and upper
+/**
+ * main - prints output alphabets in lower
  *
  * Return: Always 0
  */
@@ -11,9 +11,9 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	}
 	putchar('\n');
 
 	return (0);
