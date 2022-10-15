3cf6282feadddb85800ff6ef5c30b489ea78a062
tinashenatty <nattymachona2000@gmail.com>
2022-10-15 06:58:37 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 48; v < 58; v++)
+	{
+		putchar(t);
+		if (t != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
