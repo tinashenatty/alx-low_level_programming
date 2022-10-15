6466b4b753b22e8516655a880e99d0b0a753e8b2
tinashenatty <nattymachona2000@gmail.com>
2022-10-15 05:44:35 -0700

    Task 3 resolved

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,24 +7,18 @@
  * Return: Always 0
  */
 
-/* your code goes there */
+
 int main(void)/* your code goes there */
 {
 	char ch;
-	char ma;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'e' && ch != 'q')
-			putchar(ch);
+		putchar(ch);
 	}
-	for (ma = 'A'; ma <= 'Z'; ma++)
-	{
-		if (ma != 'E' && ma != 'Q')
-			putchar(ma);
-	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	putchar('\n');
-	/* your code goes there */
+
 
 	return (0);
 }
