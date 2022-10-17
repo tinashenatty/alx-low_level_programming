2b19652c13c9411fac677db103735c62d2089ecc
tinashenatty <nattymachona2000@gmail.com>
2022-10-17 10:14:48 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			putchar(' ');
+
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
