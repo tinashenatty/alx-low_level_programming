0cf54c61f3d621978af0cbd3f877e47f395dc8ed
tinashenatty <nattymachona2000@gmail.com>
2022-10-18 09:31:40 -0700

    Task -m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
