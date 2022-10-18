707dc013894489205b71f18133e28afb1acb3dac
tinashenatty <nattymachona2000@gmail.com>
2022-10-18 10:28:45 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints that
+ */
+void print_alphabet_x10(void);
+{
+	int num;
+	char ch;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
