4c113b20f2e3f78bb44c95d374b31f7259c39adf
tinashenatty <nattymachona2000@gmail.com>
2022-10-19 10:38:21 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long f1 = 0, f2 = 1, fsum;
+	float totalSum;
+
+	while (1)
+	{
+		fsum = f1 + f2;
+		if (fsum > 4000000)
+			break;
+
+		if ((fsum % 2) == 0)
+			totalSum += fsum;
+
+		f1 = f2;
+		f2 = fsum;
+	}
+	printf("%.0f\n", totalSum);
+
+	return (0);
+}
