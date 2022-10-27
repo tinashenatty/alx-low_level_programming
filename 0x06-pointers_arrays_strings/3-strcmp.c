a71a69d646ee10520ae2a90cbb6ec46c3bae7d24
tinashenatty <nattymachona2000@gmail.com>
2022-10-27 06:15:02 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two string values
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: difference in numerals
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+
+	return (0);
+}
