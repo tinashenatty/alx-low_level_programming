365e8aa9f994cc260d4aaec6199b2474f487fa76
tinashenatty <nattymachona2000@gmail.com>
2022-10-27 09:37:32 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,16 @@
 
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		x = -x;
 	}
 
-	x = n;
-	if (x / 10)
-		print_number(x / 100);
+	if ((x / 10) > 0)
+		print_number(x / 10);
 
-	_putchar(x % 10 + '0');
+	_putchar((x % 10) + '0');
 }
