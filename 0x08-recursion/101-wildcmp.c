d395956b602b01e2044ba6406125cb9d624a4dab
tinashenatty <nattymachona2000@gmail.com>
2022-11-02 11:23:35 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,75 @@
 #include "main.h"
 
 /**
- * str_checker - check if two strings are identical
- * @s1: string 1
- * @s2: string 2
- * @i: left index
- * @j: index poker
- * Return: 1 if s is palindrome otherwise 0
+ * strlen_no_wilds - Returns the length of a string
+ * @str: Th estring to be measured
+ *
+ * Return: Length
  */
-int str_checker(char *s1, char *s2, int i, int j)
+int strlen_no_wilds(char *str)
 {
-	if (s1[i] == '\0' && s2[j] == '\0')
-		return (1);
-	if (s1[i] == s2[j])
-		return (str_checker(s1, s2, i + 1, j + 1));
-	if (s1[i] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, i, j + 1));
-	if (s2[j] == '*')
-		return (str_checker(s1, s2. i + 1, j) || str_checker(s1, s2, i, j + 1));
-	return (0);
+	int len = 0, index = 0;
+
+	if (*(str + index))
+	{
+		if (*str != '*')
+			len++;
+		index++;
+		len += strlen_no_wilds(str + index);
+	}
+	return (len);
+}
+
+/**
+ * iterate_wild - iterates through a string
+ * @wildstr: the string to be iterated
+ */
+void iterate_wild(char **wildstr)
+{
+	if (**wildstr == '*')
+	{
+		(*wildstr)++;
+		iterate_wild(wildstr);
+	}
 }
 /**
- * wildcmp - check if string can be considered identical
- * @s1: base address
- * @s2: base address 2
+ * postfix_match - checks if a string matches postfix
+ * @str: string
+ * @postfix: Postfix
  *
- * Return: 1 if identical
+ * Return: pointer to null otherwise char in postfix
+ */
+char *postfix_match(char *str, char *postfix)
+{
+	int str_len = strlen_no_wilds(str) - 1;
+	int postfix_len = strlen_no_wilds(postfix) - 1;
+
+	if (*postfix == '*')
+		iterate_wild(&postfix);
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (postfix_match(str, postfix));
+	}
+	return (postfix);
+}
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (str_checker(s1, s2, 0, 0));
+	if (*s2 == '*')
+	{
+		iterate_wild(&s2);
+		s2 = postfix_match(s1, s2);
+	}
+	if (*s2 == '\0')
+		return (1);
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(++s1, ++s2));
 }
