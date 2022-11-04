7be8fb5cc1cab19905afb543413064fee9aab67e
tinashenatty <nattymachona2000@gmail.com>
2022-11-04 07:27:03 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: count of arguments
+ * @argv: array of argumrnts
+ *
+ * Return: Awlays 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
