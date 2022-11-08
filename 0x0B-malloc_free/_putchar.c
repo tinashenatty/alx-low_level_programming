a9740697b424604666d626e623a5329a34e034a4
tinashenatty <nattymachona2000@gmail.com>
2022-11-08 06:01:39 -0800

    Task 0

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to the stdout
+ * @c: The character to print
+ *
+ * Return: On success 1 else -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
